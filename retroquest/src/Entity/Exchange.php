acce308e46a57fc606bbd8e3084235276037524d
Guillaume DEBUCQUET <guillaume.debucquet@gmail.com>
2026-05-27 14:02:13 +0200

    création de l'entité (+ repo) Exchange (make:entity)

---
+++ retroquest/src/Entity/Exchange.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExchangeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExchangeRepository::class)]
+class Exchange
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?\DateTime $propositionDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getPropositionDate(): ?\DateTime
+    {
+        return $this->propositionDate;
+    }
+
+    public function setPropositionDate(\DateTime $propositionDate): static
+    {
+        $this->propositionDate = $propositionDate;
+
+        return $this;
+    }
+}
