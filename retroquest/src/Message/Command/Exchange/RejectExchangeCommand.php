5fe845a04a26c2751d8a25913c7d2bc19b9b5a9f
Guillaume DEBUCQUET <guillaume.debucquet@gmail.com>
2026-06-05 14:25:22 +0200

    implémentation messages exchanges

---
+++ retroquest/src/Message/Command/Exchange/RejectExchangeCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message\Command\Exchange;
+
+readonly class RejectExchangeCommand
+{
+    public function __construct(
+        public int $exchangeId,
+        public int $userId
+    ) {}
+}
---
+++ retroquest/src/MessageHandler/Command/Exchange/RejectExchangeCommandHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MessageHandler\Command\Exchange;
+
+use App\Message\Command\Exchange\RejectExchangeCommand;
+use App\Enum\ExchangeStatuses;
+use App\Exception\ExchangeNotFoundException;
+use App\Exception\UserNotFoundException;
+use App\Repository\ExchangeRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+#[AsMessageHandler]
+class RejectExchangeCommandHandler
+{
+    public function __construct(
+        private ExchangeRepository $exchangeRepository,
+        private UserRepository $userRepository,
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    public function __invoke(RejectExchangeCommand $command): void
+    {
+        $exchange = $this->exchangeRepository->find($command->exchangeId);
+        if ($exchange === null) {
+            throw new ExchangeNotFoundException('Échange introuvable.');
+        }
+
+        $user = $this->userRepository->find($command->userId);
+        if ($user === null) {
+            throw new UserNotFoundException('Utilisateur introuvable.');
+        }
+
+        if ($exchange->getReceiver() !== $user) {
+            throw new AccessDeniedException("Vous n'êtes pas autorisé à refuser cet échange.");
+        }
+
+        if ($exchange->getStatus() !== ExchangeStatuses::PENDING) {
+            throw new \LogicException("Cet échange ne peut pas être refusé car il n'est plus en attente.");
+        }
+
+        $exchange->setStatus(ExchangeStatuses::REJECTED);
+        $this->entityManager->flush();
+    }
+}
